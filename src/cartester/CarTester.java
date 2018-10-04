2ecbfb8ad9f437a196620d4f50e1bbf1896bdb00
hildana9675 <36871934+hildana9675@users.noreply.github.com>
2018-10-04 15:07:35 -0400

    Update CarTester.java

--- src/cartester/CarTester.java
+++ src/cartester/CarTester.java
@@ -1,7 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Hildana M.
+ * CarTester.java
+ * This program shows three car with different properties
+ * due: 
  */
 
 package cartester;
